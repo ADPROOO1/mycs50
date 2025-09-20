f2c891a92b824390bb159eb2caecda91ca123fd8
ADPROOO1 <adrianprofessional001@gmail.com>
2025-09-20 06:53:20 +0000

    Sat, Sep 20, 2025, 6:53 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -230,17 +230,15 @@ void print_winner(void)
     // Get all candidates
     for (int i = 0; i < candidate_count; i++)
     {
+        if (winning_candidates[i].score == 0)
+        {
+            winning_candidates[i].score = candidate_count;
+        }
         for (int j = 0; j < candidate_count; j++)
         {
             if (locked[i][j])
             {
-                winning_candidates[i].score++;
-                {
-                    if (winning_candidates[j].score > 0)
-                    {
-                        winning_candidates[j].score--;
-                    }
-                }
+                winning_candidates[j].score--;
             }
         }
     }
